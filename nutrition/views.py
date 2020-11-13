d83d0375ba429d0f67764b91ead9530662e1ca4e
Tilly3g <tilly.gee@hotmail.co.uk>
2020-11-13 13:55:00 +0000

    Add view for food diary (nutrition)

--- nutrition/views.py
+++ nutrition/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Nutrition
 
 # Create your views here.
+
+
+def all_nutrition(request):
+
+    foods = Nutrition.objects.all()
+
+    context = {
+        'foods': foods,
+    }
+
+    return render(request, 'nutrition/nutrition.html', context)
