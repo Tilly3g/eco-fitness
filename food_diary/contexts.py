ec50b0ecaf2d6fb3d532b5e9ce9295d73f037e59
Tilly3g <tilly.gee@hotmail.co.uk>
2020-11-16 18:58:20 +0000

    Update diary to include calories and calories remaining

--- food_diary/contexts.py
+++ food_diary/contexts.py
@@ -8,32 +8,38 @@ def get_food(request):
     calories = 0
     food_amount = 0
     goal = 2000
+    total_calories = 0
     diary = request.session.get('food_diary', {})
 
     for food_id, food_data in diary.items():
         if isinstance(food_data, int):
             food = get_object_or_404(Nutrition, pk=food_id)
-            calories += food.Calories / food.Grams * food_data
+            calories = food.Calories / food.Grams * food_data
+            total_calories += calories
+            food_amount += food_data
             food_items.append({
                 'food_id': food_id,
                 'amount': food_data,
                 'food': food,
+                'calories': calories,
             })
         else:
             food = get_object_or_404(Nutrition, pk=food_id)
             for amount in food_data[food_id].items():
-                calories += food.Calories / food.Grams * amount
+                calories = food.Calories / food.Grams * amount
+                total_calories += calories
                 food_amount += amount
                 food_items.append({
                     'food_id': food_id,
                     'amount': amount,
                     'food': food,
+                    'calories': calories,
                 })
 
-    if calories < goal:
-        calories_left = goal - calories
+    if total_calories < goal:
+        calories_left = int(goal - total_calories)
     else:
-        calories_left = "You have reached your calorie gaol for snacks today."
+        calories_left = "You have reached your calorie goal for today."
 
     context = {
         'food_items': food_items,
