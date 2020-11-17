dd0ed8f8e22447111e838d37aa07d3de1660d4df
Tilly3g <tilly.gee@hotmail.co.uk>
2020-11-17 14:46:39 +0000

    add signals for payment app

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Payment, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'order_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Payment, PaymentAdmin)
