437a21907c7be67b68dadb0748034dc069908c64
Tilly3g <tilly.gee@hotmail.co.uk>
2020-11-17 14:58:37 +0000

    add payment views and urls

--- eco_fitness/urls.py
+++ eco_fitness/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('bookings/', include('bookings.urls')),
     path('diary/', include('food_diary.urls')),
     path('bag/', include('bag.urls')),
+    path('payment/', include('payments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import PaymentForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Looks like you haven't added any sessions to your bag")
+        return redirect(reverse('bookings'))
+
+    payment_form = PaymentForm()
+    template = 'payments/payment.html'
+    context = {
+        'payment_form': payment_form,
+    }
+
+    return render(request, template, context)
