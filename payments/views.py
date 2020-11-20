eb39c90a2e01a9430f3d6a71db6059b13b3a3de3
Tilly3g <tilly.gee@hotmail.co.uk>
2020-11-20 15:16:06 +0000

    add payment view to save billing info on success if checked

--- payments/views.py
+++ payments/views.py
@@ -1,32 +1,89 @@
+
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from bag.contexts import bag_contents
-from .models import Payment
 from django.conf import settings
 
 from .forms import PaymentForm
+from bag.contexts import bag_contents
+from .models import Payment, OrderLineItem
+from bookings.models import Session
+
 import stripe
+import json
 
 
 def payment(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "Looks like you haven't added any sessions to your bag")
-        return redirect(reverse('bookings'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
 
-    bag = bag_contents(request)
-    total = bag['total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'postcode': request.POST['postcode'],
+            'county': request.POST['county'],
+        }
+        payment_form = PaymentForm(form_data)
+        if payment_form.is_valid():
+            order = payment_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
+            for booking_id, booking_data in bag.items():
+                try:
+                    booking = Session.objects.get(id=booking_id)
+                    if isinstance(booking_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            booking=booking,
+                            quantity=booking_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for quantity in booking_data.items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                booking=booking,
+                                quantity=quantity,
+                            )
+                            order_line_item.save()
+                except Session.DoesNotExist:
+                    messages.error(request, (
+                        "One of the session types wasn't found in our database. "
+                        "Contact us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
 
-    payment_form = PaymentForm()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('payment_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your payment. \
+                Please check the information you have entered.')
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "Looks like you haven't added any sessions to your bag")
+            return redirect(reverse('bookings'))
+
+        bag = bag_contents(request)
+        total = bag['total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        payment_form = PaymentForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -46,10 +103,11 @@ def payment_success(request, order_number):
     """
     Successful payments
     """
+    save_info = request.session.get('save_info')
     booking = get_object_or_404(Payment, order_number=order_number)
     messages.success(request, f'Booking successfully processed! \
         Your order number is {order_number}. A confirmation \
-        email will be sent to {booking.email} and your Expert will be in touch shortly to book in a time and date.')
+        email will be sent to {booking.email} and your Expert will be in touch shortly by phone or email to book in a time and date.')
 
     if 'bag' in request.session:
         del request.session['bag']
