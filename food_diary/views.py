fa6e6ccbc9971f5768c6480db1fa6d182a340bfa
Tilly3g <tilly.gee@hotmail.co.uk>
2020-11-24 20:15:33 +0000

    styled nutritional info page

--- food_diary/views.py
+++ food_diary/views.py
@@ -14,14 +14,17 @@ def view_diary(request):
 def add_to_diary(request, food_id):
     """ Add amount of the specified food to the diary """
 
+    food = get_object_or_404(Nutrition, pk=food_id)
     amount = int(request.POST.get('amount'))
     redirect_url = request.POST.get('redirect_url')
     diary = request.session.get('food_diary', {})
 
     if food_id in list(diary.keys()):
         diary[food_id] += amount
+        messages.success(request, f'Updated {food.Food} amount to {diary[food_id]}g')
     else:
         diary[food_id] = amount
+        messages.success(request, f'Added {food.Food} to your diary')
 
     request.session['food_diary'] = diary
     return redirect(redirect_url)
