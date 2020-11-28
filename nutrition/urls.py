62c057afdd979c2a6be3f0abc92dd87f1aed6c09
Tilly3g <tilly.gee@hotmail.co.uk>
2020-11-28 11:55:43 +0000

    style allauth templates to match site

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.all_nutrition, name='nutrition'),
     path('<food_id>', views.food_item, name='food_item'),
-    path('add/', views.add_nutrition, name='add_nutriton'),
+    path('add/', views.add_nutrition, name='add_nutrition'),
     path('edit/<int:food_id>/', views.edit_nutrition, name='edit_nutrition'),
     path('delete/<int:food_id>/', views.delete_nutrition, name='delete_nutrition'),
 ]
