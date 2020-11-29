58ba6eb1dc5d4718a3c35e1910aaade24fc9ce58
Tilly3g <tilly.gee@hotmail.co.uk>
2020-11-29 11:54:00 +0000

    update django secret key

--- eco_fitness/settings.py
+++ eco_fitness/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_KEY', 'mongodb://localhost')
+SECRET_KEY = os.getenv('DJANGO_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['eco-fitness.herokuapp.com', 'localhost']
 
