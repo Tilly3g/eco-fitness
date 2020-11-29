e36b62ddb9feb86909b3bc7b85871dd36a1ae45b
Tilly3g <tilly.gee@hotmail.co.uk>
2020-11-29 11:27:18 +0000

    Fix error when changing DB back to Django

--- eco_fitness/settings.py
+++ eco_fitness/settings.py
@@ -117,10 +117,10 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'eco_fitness.wsgi.application'
 
 
-Database
- https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
